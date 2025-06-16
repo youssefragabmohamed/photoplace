47cb6e4318ad67c6b4865a6302a17b68ea9052dd
donut.king <donut.king992@gmail.com>
2025-06-16 23:56:43 +0300

    Added Comment model for photo comments

---
+++ models/comment.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  photoId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Photo',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 500
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Index for faster queries
+commentSchema.index({ photoId: 1, createdAt: -1 });
+commentSchema.index({ userId: 1, createdAt: -1 });
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment; 
